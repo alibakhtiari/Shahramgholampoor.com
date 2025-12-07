11c1af962c5b181a6a48e606f3dfb8b2dcaa7a34
Ali Bakhtiari <ali.bakhtiarii@gmail.com>
2025-12-07 10:45:30 +0330

    feat: complete final polish and SEO implementation

---
+++ src/data/videos.ts
@@ -0,0 +1,84 @@
+import vid2022Poster from "../assets/vid/2022.jpg";
+import vid2022Thumb from "../assets/vid/2022.t.jpg";
+import vid2018_6Poster from "../assets/vid/2018.6.jpg";
+import vid2018_6Thumb from "../assets/vid/2018.6t.jpg";
+import vid2018_5Poster from "../assets/vid/2018.5.jpg";
+import vid2018_5Thumb from "../assets/vid/2018.5t.jpg";
+import vid2018_4Poster from "../assets/vid/2018.4.jpg";
+import vid2018_4Thumb from "../assets/vid/2018.4t.jpg";
+import vid2018_3Poster from "../assets/vid/2018.3.jpg";
+import vid2018_3Thumb from "../assets/vid/2018.3t.jpg";
+import vid2018_2Poster from "../assets/vid/2018.2.jpg";
+import vid2018_2Thumb from "../assets/vid/2018.2t.jpg";
+import vid2017Poster from "../assets/vid/2017.jpg";
+import vid2017Thumb from "../assets/vid/2017t.jpg";
+import vid2008Poster from "../assets/vid/2008.jpg";
+import vid2008Thumb from "../assets/vid/2008t.jpg";
+import vid1998Poster from "../assets/vid/1998.jpg";
+import vid1998Thumb from "../assets/vid/1998t.jpg";
+
+export const videos = [
+    {
+        id: "2022",
+        title: "2022",
+        src: "https://raw.githubusercontent.com/alibakhtiari/Shahramgholampoor.com/main/assets/vid/2022.mp4",
+        thumb: vid2022Thumb,
+        poster: vid2022Poster,
+    },
+    {
+        id: "2018.6",
+        title: "2018.6",
+        src: "https://raw.githubusercontent.com/alibakhtiari/Shahramgholampoor.com/main/assets/vid/2018.6.mp4",
+        thumb: vid2018_6Thumb,
+        poster: vid2018_6Poster,
+    },
+    {
+        id: "2018.5",
+        title: "2018.5",
+        src: "https://raw.githubusercontent.com/alibakhtiari/Shahramgholampoor.com/main/assets/vid/2018.5.mp4",
+        thumb: vid2018_5Thumb,
+        poster: vid2018_5Poster,
+    },
+    {
+        id: "2018.4",
+        title: "2018.4",
+        src: "https://raw.githubusercontent.com/alibakhtiari/Shahramgholampoor.com/main/assets/vid/2018.4.mp4",
+        thumb: vid2018_4Thumb,
+        poster: vid2018_4Poster,
+    },
+    {
+        id: "2018.3",
+        title: "2018.3",
+        src: "https://raw.githubusercontent.com/alibakhtiari/Shahramgholampoor.com/main/assets/vid/2018.3.mp4",
+        thumb: vid2018_3Thumb,
+        poster: vid2018_3Poster,
+    },
+    {
+        id: "2018.2",
+        title: "2018.2",
+        src: "https://raw.githubusercontent.com/alibakhtiari/Shahramgholampoor.com/main/assets/vid/2018.2.mp4",
+        thumb: vid2018_2Thumb,
+        poster: vid2018_2Poster,
+    },
+    {
+        id: "2017",
+        title: "2017",
+        src: "/assets/vid/2017.mp4",
+        thumb: vid2017Thumb,
+        poster: vid2017Poster,
+    },
+    {
+        id: "2008",
+        title: "2008",
+        src: "/assets/vid/2008.mp4",
+        thumb: vid2008Thumb,
+        poster: vid2008Poster,
+    },
+    {
+        id: "1998",
+        title: "1998",
+        src: "/assets/vid/1998.mp4",
+        thumb: vid1998Thumb,
+        poster: vid1998Poster,
+    },
+];
